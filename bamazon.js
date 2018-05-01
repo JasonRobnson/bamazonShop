36ba90212bd0dfff2fb12fdb004f57a93a6e97e0
unknown <bubbastinx@gmail.com>
2018-05-01 12:08:31 -0600

    upon running node, table will show with inventory and quantity, all working as expected

---
+++ bamazon.js
@@ -0,0 +1,36 @@
+let mysql = require("mysql");
+let inquirer = require("inquirer");
+require ("console.table");
+
+
+// create the connection information for the sql database
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon_DB"
+  });
+
+
+  // connect to the mysql server and sql database
+connection.connect(function(err) {
+    console.log("You're connected as id " + connection.threadId);
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+connection.end();
+});
+
+
+// Start function that will initiate the start of code, will log DB resluts
+function start(){
+    connection.query("SELECT * FROM bamazonShop", function(err, results){
+        if (err) throw err;
+        console.table(results);
+    })
+}
