90fdbb689952b88b2d128569d2f578ca2c43023b
unknown <bubbastinx@gmail.com>
2018-05-03 11:53:41 -0600

    Starting development for manager view of inventory

---
+++ bamazonManager.js
@@ -0,0 +1,26 @@
+let mysql = require("mysql");
+let inquirer = require("inquirer")
+require("console.table")
+
+// create DB, then connect DB information for the sql database
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon_DB"
+  });
+
+  connection.connect(function(err) {
+      console.log("You're connected as id " + connection.threadId);
+      if (err) throw err
+      start();
+  })
+
+  function start() {
+      console.log("You're starting the app!")
+  }
