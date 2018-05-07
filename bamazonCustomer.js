a4efca668e46448586a3f6512d3db694e77bf951
unknown <bubbastinx@gmail.com>
2018-05-07 14:10:42 -0600

    added user input validator for item_id, will reject any id number that is not in the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,43 +1,43 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
-require ("console.table");
+require("console.table");
 
 
 // create the connection information for the sql database
 let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "bamazon_DB"
-  });
+});
 
 
-  // connect to the mysql server and sql database
-connection.connect(function(err) {
+// connect to the mysql server and sql database
+connection.connect(function (err) {
     console.log("You're connected as id " + connection.threadId);
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 
- 
+
 
 });
 
 
 // Start function that will initiate the start of code, will log DB resluts
-function start(){
-    connection.query("SELECT * FROM bamazonShop", function(err, results){
+function start() {
+    connection.query("SELECT * FROM bamazonShop", function (err, results) {
         if (err) throw err;
         console.table(results);
         customerSearch();
-        
-          })
-   
+
+    })
+
 }
 
 function customerSearch() {
@@ -45,57 +45,78 @@ function customerSearch() {
         name: 'customerSearchId',
         type: 'input',
         message: "What is the ID of the product that you're looking ot buy?",
-    }).then(function(answer){
+    }).then(function (answer) {
         let customerProductID = answer.customerSearchId;
         inquirer.prompt({
             name: 'customerProductQuantity',
             type: 'input',
             message: "How many units of this product would you like to buy?",
-        }).then(function(answer){
+        }).then(function (answer) {
             let customerProductQuant = answer.customerProductQuantity;
             checkInventory(customerProductID, customerProductQuant);
-            })
+        })
 
-    }) 
+    })
 }
 
-function checkInventory (productID, productQuant) {
-    connection.query(`SELECT * FROM  bamazonShop WHERE item_id = '${productID}'`, function(err, results){
+function checkInventory(productID, productQuant) {
+    let itemIdValue = Boolean(itemIdValidator(productID))
+        if (itemIdValue === false){
+            endOrder;
+        } else {
+    
+    let IntegerProductQuant = parseInt(productQuant);
+
+    connection.query(`SELECT * FROM  bamazonShop WHERE item_id = '${productID}'`, function (err, results) {
         if (err) throw err;
-        let IntegerProductQuant = parseInt(productQuant);
-            //if statements test product order quantity against customer quantity
-        if (IntegerProductQuant > parseInt(results[0].stock_quantity)){
-             console.log("Sorry we don't have that product in Stock. Please Try Back Later");
-             endOrder();
-        } 
-        else {
-            if (IntegerProductQuant < 0 ){
+
+        //if statements test product order quantity against customer quantity
+        if (IntegerProductQuant > parseInt(results[0].stock_quantity)) {
+            console.log("Sorry we don't have that product in Stock. Please Try Back Later");
+            endOrder();
+        } else {
+            if (IntegerProductQuant < 0) {
                 console.log("Sorry orders must be for 1 or more items.")
                 endOrder();
+            } else {
+                //does basic arithmitc and sets stock quantity to a new variable
+                let inventoryUpdateValue = (parseInt(results[0].stock_quantity)) - productQuant
+                connection.query(`UPDATE bamazonshop SET stock_quantity='${inventoryUpdateValue}' Where item_id='${productID}'`, function (err, results) {
+                    productCostPrice(productID, productQuant);
+
+                })
             }
-            else {
-             //does basic arithmitc and sets stock quantity to a new variable
-             let inventoryUpdateValue = (parseInt(results[0].stock_quantity)) - productQuant
-               connection.query(`UPDATE bamazonshop SET stock_quantity='${inventoryUpdateValue}' Where item_id='${productID}'`, function(err, results){
-                productCostPrice(productID, productQuant);
-            })
         }
-    }
- })
+    })
+}
+endOrder();
 }
 
 
-function productCostPrice (productID, productQuant){
-    connection.query(`SELECT * FROM  bamazonShop WHERE item_id = '${productID}'`, function(err, results){
+
+function productCostPrice(productID, productQuant) {
+    connection.query(`SELECT * FROM  bamazonShop WHERE item_id = '${productID}'`, function (err, results) {
         if (err) throw err;
-        console.log(`Your `+results[0].product_name + ` cost $`+ results[0].price + ' per item.');
+        console.log(`Your ` + results[0].product_name + ` cost $` + results[0].price + ' per item.');
         let customerPrice = parseInt(productQuant) * parseInt(results[0].price);
-        console.log(`Your oder total is $`+ customerPrice);
+        console.log(`Your oder total is $` + customerPrice);
         endOrder();
     })
 
 }
+function itemIdValidator(productID) {
+    connection.query('SELECT item_id FROM bamazonshop ORDER by(item_id+0) DESC', function(err, results){
+        if (err) throw err;
+        if (productID >results[0].item_id) {
+            console.log("Please chose an Item_Id listed on the table...")
+            return false
+            endOrder();
+        }
 
-function endOrder(){
+    })
+
+}
+
+function endOrder() {
     connection.end();
 }
